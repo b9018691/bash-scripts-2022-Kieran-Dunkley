ff3130963217f35bb3cceff64d754ed93c571027
b9018691 <120064134+b9018691@users.noreply.github.com>
2022-12-08 08:56:44 +0000

    Add files via upload
    read check script creation and other bash script edits

--- create_directory.sh
+++ create_directory.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 read -p "Enter directory name: " DIRECTORY
-echo "directory named $DIRECTORY will be made."
-mkdir $DIRECTORY
+if [ -d "$DIRECTORY" ]
+then 
+	echo "$DIRECTORY Dicrectory all ready exists"
+elif	[ -d "$DIRECTORY" ]
+then 
+	echo "directory named $DIRECTORY will be made."
+	mkdir "$DIRECTORY"
+fi
 
+ls -l "$DIRECTORY"
--- find-files.sh
+++ find-files.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 read -p "Enter the path of the file or directory: " FILE
- if [-f "$FILE"]
+ if [ -f "$FILE" ]
 	then echo "$FILE is a regular file."
- elif [-d "$FILE"]
-	echo "$FILE is directory"
+ elif [ -d "$FILE" ]
+        then echo "$FILE is A directory"
  else
  	echo "$FILE no such file ordirectory exisits "
  fi
 
-ls -l $FILE
+ls -l "$FILE"
